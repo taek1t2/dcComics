fab5a9b7d60a99f87cfeaa479232cf2635fc7004
Taeyeon Kim <taeyeon.n.k@gmail.com>
2019-03-27 12:24:11 -0600

    Working on jQuery

---
+++ assets/js/game.js
@@ -0,0 +1,15 @@
+//global variables
+let characterOptions= ["Wonder Woman", "Superman", "Cyborg", "Batman"];
+let chooseCharacter = [];
+let sparringPartner = [];
+let wins = 0;
+let losses = 0;
+let battleZone = [];
+
+
+//start the game
+$(document).ready(function() {
+    //Choose a character: 
+    
+
+})
